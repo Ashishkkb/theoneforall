3188ff55dd0a82dd299004cfbc8a0ca54291ad90
Ashish Kumar Behera <ashish.behera@aniccadata.in>
2023-11-08 17:21:59 +0530

    Mouse Follower

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../styles/global.scss';
+import {useEffect} from 'react'
+const mouseFollower = import('../utils/mouseFollower').then((module) => {
+  return module.default;
+});
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
---
+++ utils/mouseFollower.ts
@@ -0,0 +1,10 @@
+import MouseFollower from 'mouse-follower';
+import gsap from 'gsap';
+
+
+MouseFollower.registerGSAP(gsap);
+
+const mouseFollower = new MouseFollower();
+
+
+export default mouseFollower
